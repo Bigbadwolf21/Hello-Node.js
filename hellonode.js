20f2c5cf4bdf5edf183b30797ce91a8e12e2453e
Ayodele Ayomide <66219396+Bigbadwolf21@users.noreply.github.com>
2022-04-29 20:12:07 +0100

    Add files via upload

---
+++ hellonode.js
@@ -0,0 +1,28 @@
+const firstName = 'Ayomide';
+const lastName = 'Ayodele';
+let age = 23;
+const course = 'Economics';
+let school = 'Adekunle Ajasin University, Ondo State';
+let doingAtAsuuStrikeFest = 'Software Engineering';
+const where = 'ALX';
+let interests = 'Technology, Christian philosophy, Movies, Books';
+let roles = 'Data science, Software engineering';
+let http = require("http");
+
+// Create Server
+http.createServer(function (request, response) {
+    /** 
+     * Send the HTTP header
+     * HTTP Status: 200 : Ok
+     * Content Type: text/plain
+     */ 
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    // Send response body
+    response.end(`Hello World!\n Guess who is new to Backend development with Node.js\n Me! ${firstName} ${lastName} I am ${age}years old, I am currently studying ${course} at ${school}\n I am currently building skills in ${doingAtAsuuStrikeFest} at ${where}.\n I am interested in ${interests}, and I am currently open to mentorship and internships in ${roles} roles.`);
+   // response.end('Guess who is new to Backend development with Node.js\n');
+   // response.end(`Me! ${firstName} ${lastName} I am ${age}years old, I am currently studying ${course} at ${school}\n`);
+   // response.end(`I am currently building skills in ${doingAtAsuuStrikeFest} at ${where}.\n I am interested in ${interests}, and I am currently open to mentorship and internships in ${roles} roles.`);
+}).listen(8081);
+// log server info to the console
+console.log('Server running at http://127.0.0.1:8081/');
+

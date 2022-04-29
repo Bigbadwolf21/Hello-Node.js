b4713cc86b5aaa6e45cc0ce5de0fad63d017a9f7
Ayodele Ayomide <66219396+Bigbadwolf21@users.noreply.github.com>
2022-04-29 20:10:12 +0100

    Add files via upload

---
+++ module.js
@@ -0,0 +1,14 @@
+function sonFightingStyle() {
+    console.log('Kungfu');
+}
+
+function deleFightingStyle() {
+    console.log('Karate');
+}
+
+function dejiFightingStyle() {
+    console.log('Taekwando');
+}
+
+
+module.exports.sonFightingStyle = sonFightingStyle;
